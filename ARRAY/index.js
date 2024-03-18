ddd9ef08b0270e9c8624e3b7aced76ddbf73da60
wein1234 <163838661+wein1234@users.noreply.github.com>
2024-03-18 03:12:23 -0700

    Add files via upload

---
+++ ARRAY/index.js
@@ -0,0 +1,18 @@
+//lets get the value of 9
+var a=4;
+var b=5;
+var c =a + b;
+window.alert(c);
+// value of 10
+var go=6;
+var wet=4;
+var arm=go+wet;
+document.getElementById("ant").innerHTML=arm;
+document.getElementById("demo").src="ROAD.jpg";
+document.getElementById("are").innerHTML="How Are You";
+var username,password,age;
+username="joy";
+password="usman";
+age=50;
+var alluser="personsusername"+" "+ username + "<br/>" + "personspassword" + " "+ password + "<br />"+ "personsage"+" "+age;
+document.write(alluser);
